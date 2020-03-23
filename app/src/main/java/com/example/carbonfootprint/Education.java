a87039291c1b534987a9ac14a669c9af0c548164
Abigail Young <abigail.young36@gmail.com>
2020-03-23 16:09:06 +0000

    creating scrollview for tips page

--- app/src/main/java/com/example/carbonfootprint/Education.java
+++ app/src/main/java/com/example/carbonfootprint/Education.java
@@ -1,11 +1,16 @@
 package com.example.carbonfootprint;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.TextView;
 
 public class Education extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
