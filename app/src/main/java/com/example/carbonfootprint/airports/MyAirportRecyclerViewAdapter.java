cf8a4ca90596b63a2413c674b89c8fd3db7fc47e
alistair <fjb19161@uni.strath.ac.uk>
2020-03-29 21:27:07 +0100

    Adds functionality for the flight calculator if you run it as its own activity, but the UI is still pretty broken.

--- app/src/main/java/com/example/carbonfootprint/airports/MyAirportRecyclerViewAdapter.java
+++ app/src/main/java/com/example/carbonfootprint/airports/MyAirportRecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package com.example.carbonfootprint.airports;
 
-import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -19,10 +18,12 @@ public class MyAirportRecyclerViewAdapter extends RecyclerView.Adapter<MyAirport
 
     private List<Airport> mValues;
     private LayoutInflater mInflater;
+    private View.OnClickListener mListener;
 
 
-    public MyAirportRecyclerViewAdapter(Context context) {
+    public MyAirportRecyclerViewAdapter(Context context, View.OnClickListener listener) {
         mInflater = LayoutInflater.from(context);
+        mListener = listener;
     }
 
 
@@ -31,16 +32,18 @@ public class MyAirportRecyclerViewAdapter extends RecyclerView.Adapter<MyAirport
     public AirportViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = mInflater
                 .inflate(R.layout.fragment_airport, parent, false);
+                view.setOnClickListener(mListener);
+
         return new AirportViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(AirportViewHolder holder, int position) {
         if(mValues != null) {
-        //    holder.airportName.setText(mValues.get(position).name);
             holder.airportCode.setText(mValues.get(position).iata);
+            }
         }
-    }
+
 
     @Override
     public int getItemCount() {
@@ -48,23 +51,23 @@ public class MyAirportRecyclerViewAdapter extends RecyclerView.Adapter<MyAirport
         else return 0;
     }
 
+    public void setValues(List<Airport> airports){
+        mValues = airports;
+        notifyDataSetChanged();
+        Log.d("fug",String.valueOf(getItemCount()));
+        Log.d("fug", String.valueOf(mValues.get(1).getIata()));
+    }
+
 
     class AirportViewHolder extends RecyclerView.ViewHolder {
         private final TextView airportName;
         private final TextView airportCode;
 
-        private AirportViewHolder(View view) {
-            super(view);
-            airportName = (TextView) view.findViewById(R.id.airport_name);
-            airportCode = (TextView) view.findViewById(R.id.airport_code);
-
+        private AirportViewHolder(View itemView) {
+            super(itemView);
+            airportName = (TextView) itemView.findViewById(R.id.airport_name);
+            airportCode = (TextView) itemView.findViewById(R.id.airport_code);
         }
     }
 
-        public void setValues(List<Airport> airports){
-            mValues = airports;
-            notifyDataSetChanged();
-            Log.d("fug",String.valueOf(getItemCount()));
-            Log.d("fug", String.valueOf(mValues.get(1).getIata()));
-        }
     }
